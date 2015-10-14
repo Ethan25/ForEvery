7b23aeb2cb7d7e3f304a3bc64edb3f12c8ee0648
Ethan25 <ithan96@gmail.com>
2015-10-14 18:03:33 -0400

    Added content to the files

---
+++ main.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>ForEvery</Title>
+
+	<meta charset = "utf-8"/>
+	<meta http-equiv = "Content-type" content = "text/html; charset = utf-8"/>
+	<meta name = "viewport" content = "width = device-width, initial-scale = 1"/>
+
+	<script type = "text/javascript" src = "jquery.min.js"></script>
+  	
+  	<script type = "text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+	
+	<link rel="stylesheet" type="text/css" href="cmain.css"></style>
+
+</head>
+<body>
+
+	<div id = "menu" class = "unselectable">
+		<div id = "menumain"><a href = "">ForEvery</a></div>
+		<ul>
+			<li><a href = "">Home</a></li>
+			<li><a href = "">Account</a></li>
+		</ul>
+	</div>
+
+</body>
+</html>
